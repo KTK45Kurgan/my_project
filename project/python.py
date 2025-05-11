46def7b376c2b114218aad1390c154ea9cea1e9f
KTK45Kurgan <mitea024@gmail.com>
2025-05-12 01:07:28 +0500

    Update python.py

--- project/python.py
+++ project/python.py
@@ -1,1 +1,15 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("Люблю Python!")
+root.geometry("300x200")  
+
+
+root.configure(bg='lightblue')
+
+label = tk.Label(root, text="Я люблю Python!", font=("Helvetica", 24, "bold"), bg='lightblue', fg='darkblue')
+label.pack(expand=True)  
+
+root.mainloop()
 
